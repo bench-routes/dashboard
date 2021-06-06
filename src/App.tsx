0042871c9d755e39a953e87b2df704763ef5bdc5
ruddi <rudrakshaggarwalsachin@gmail.com>
2021-06-06 13:08:13 +0530

    Update boilerplate
    Signed-off-by: ruddi <rudrakshaggarwalsachin@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import BasicLayout from "./layouts/BasicLayout";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import Home from "./pages/Home";
-import Config from "./pages/Config";
-import { theme } from "./utils/theme/theme";
 
 export const App: React.FC = () => (
-  <ChakraProvider theme={theme}>
+  <ChakraProvider>
     <Router>
       <BasicLayout>
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-          <Route exact path="/config">
-            <Config />
-          </Route>
+          <Redirect to="/" />
         </Switch>
       </BasicLayout>
     </Router>
