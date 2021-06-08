148752ea0bbf2e835bf8128d477101e4e4dcb93f
ruddi <rudrakshaggarwalsachin@gmail.com>
2021-06-08 19:20:53 +0530

    Change extension of theme files from tsx to ts

--- src/theme/branding.ts
+++ src/theme/branding.ts
@@ -1,14 +1,24 @@
 const branding = {
   colors: {
+    //fill colors
     lightPrimary: "#e5e5e5",
     lightSecondary: "#f7f7f7",
-    lightText: "#666666",
     darkPrimary: "#292929",
     darkSecondary: "#121212",
+
+    //text colors
+    lightText: "#666666",
   },
   fonts: {
     body: "Open Sans",
     heading: "Open Sans",
+  },
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "0.925rem",
+    lg: "1rem",
+    xl: "1.125rem",
   },
 };
 
