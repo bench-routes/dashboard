c50e860ce0ff38109c2ae7b86d83b7493a8c398d
ruddi <rudrakshaggarwalsachin@gmail.com>
2021-06-14 16:21:24 +0530

    Modify fetch service
    Signed-off-by: ruddi <rudrakshaggarwalsachin@gmail.com>

--- src/utils/useFetch.ts
+++ src/utils/useFetch.ts
@@ -1,44 +1,99 @@
 /* eslint-disable */
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useEffect, useReducer, useRef } from "react";
+import axios, { AxiosRequestConfig } from "axios";
 
-export type APIResponse<T> = { statusText: string; data?: T };
-
-export interface FetchState<T> {
-  response: APIResponse<T>;
-  error?: Error;
-  isLoaded: boolean;
+interface Response<T> {
+  status: "init" | "fetching" | "error" | "fetched";
+  data?: T | undefined;
+  error?: string | undefined;
 }
 
-type ObjectLiteral = { [key: string]: any };
+// used if you want to reference
+// persistent storage
+interface Cache<T> {
+  [url: string]: T;
+}
+
+type Action<T> =
+  | { type: "request" }
+  | { type: "success"; payload: T }
+  | { type: "failure"; payload: string };
 
 /*  Reusable fetch service for making requests to api endpoints 
-    input - url
-    output - response,error,isLoaded
+    input - url,options
+    output - data,error,status
 */
-export const useFetch = <T extends ObjectLiteral>(
-  url: string
-): FetchState<T> => {
-  const [response, setResponse] = useState<APIResponse<T>>({
-    statusText: "start fetching",
-  });
-  const [error, setError] = useState<Error>();
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+const useFetch = <T = unknown>(
+  url: string,
+  options?: AxiosRequestConfig
+): Response<T> => {
+  const cache = useRef<Cache<T>>({});
+  const cancelRequest = useRef<boolean>(false);
 
+  const initialState: Response<T> = {
+    status: "init",
+    error: undefined,
+    data: undefined,
+  };
+
+  // main function
+  const fetchReducer = (state: Response<T>, action: Action<T>): Response<T> => {
+    switch (action.type) {
+      case "request":
+        return { ...initialState, status: "fetching" };
+      case "success":
+        return { ...initialState, status: "fetched", data: action.payload };
+      case "failure":
+        return { ...initialState, status: "error", error: action.payload };
+      default:
+        return state;
+    }
+  };
+
+  // useReducer instead of use state could make the function
+  // easier to debug across multiple usages.
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
   useEffect(() => {
+    if (!url) {
+      return;
+    }
+
     const fetchData = async () => {
-      try {
-        setIsLoaded(false);
-        const res = await axios.get(url);
-        const json = res as APIResponse<T>;
-        setResponse(json);
-        setIsLoaded(true);
-      } catch (error) {
-        setError(error);
+      dispatch({ type: "request" });
+
+      if (cache.current[url]) {
+        dispatch({ type: "success", payload: cache.current[url] });
+      } else {
+        try {
+          const response = options
+            ? await axios.get(url, options)
+            : await axios.get(url);
+
+          cache.current[url] = response.data;
+
+          if (cancelRequest.current) return;
+
+          dispatch({ type: "success", payload: response.data });
+        } catch (error: any) {
+          // Let's keep error handling to another day,
+          // hence the type for error is "any" here.
+          if (cancelRequest.current) return;
+
+          // server should return a message, else use a custom message such as "something went wrong"
+          dispatch({ type: "failure", payload: error.message });
+        }
       }
     };
+
     fetchData();
+
+    // runs when component is destroyed.
+    return () => {
+      cancelRequest.current = true;
+    };
   }, [url]);
 
-  return { response, error, isLoaded };
+  return state;
 };
+
+export default useFetch;
